f19de5e1930ddcc9937bec241c049b8d5337e85c
paydaylight <maxat235@gmail.com>
2020-04-15 13:50:41 +0600

    added protobuf parse

---
+++ worker.py
@@ -0,0 +1,24 @@
+import pika
+import time
+from proto_types_pb2 import Document
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='documents_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    doc = Document()
+    doc.ParseFromString(body)
+    # TODO
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='documents_queue', on_message_callback=callback)
+
+channel.start_consuming()
