ca923dbb3ffc13d5fddf0fab2705b8c0f32f6527
paydaylight <maxat235@gmail.com>
2020-05-27 00:21:25 +0600

    fix

--- bert/entrypoint.sh
+++ bert/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bert-serving-start -http_port 8125 -num_worker=2 -model_dir /model/uncased_L-12_H-768_A-12/ -cpu -max_batch_size 16 -max_seq_len 100
+bert-serving-start -http_port 8125 -num_worker=1 -model_dir /model/uncased_L-12_H-768_A-12/ -cpu -max_batch_size 16 -max_seq_len 100
