4567c3f0469cfe0157d472333bd71f5dda33912c
Daler <daler1912@gmail.com>
2020-05-27 04:41:47 +0600

    Added top 5 results

--- worker.py
+++ worker.py
@@ -1,18 +1,34 @@
 import pika
 from proto_types_pb2 import Document, DocumentsSubmission
 from bert_serving.client import BertClient
+from sklearn.metrics.pairwise import cosine_similarity
 import os
 import sseclient
 import requests
 import json
 import utils
+import numpy as np
 
-ENCODED_ABSTRACTS_URL = os.environ['ENCODED_ABSTRACTS_URL']
+# ENCODED_ABSTRACTS_URL = os.environ['ENCODED_ABSTRACTS_URL']
 
 creds = pika.credentials.PlainCredentials(
     os.environ['RABBITMQ_DEFAULT_USER'],
     os.environ['RABBITMQ_DEFAULT_PASS'], erase_on_connect=False
 )
+
+
+def get_data():
+    with open('submissions.json') as f:
+        data = json.loads(f.read())
+        # print(data[0]['id'])
+        # print(data[0]['encoded_abstract']['tokens'])
+    return np.asarray(data)
+
+data = get_data()
+
+# print(data[0])
+
+# print(len(data))
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.environ['AMQP_URL'],
                                                                credentials=creds))
@@ -22,16 +38,47 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=os.environ['
 channel = connection.channel()
 
 channel.queue_declare(queue='documents_queue', durable=True)
+
+# print(type(np.asarray(data[0]['encoded_abstract']['tokens'], dtype=np.float32)))
+
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
+bert_client = BertClient(ip="bert-server")
+
+abstract = np.asarray(bert_client.encode(test, show_tokens=False, is_tokenized=False))
+
+cos_sim = cosine_similarity(abstract.reshape(1,-1), np.asarray(data[0]['encoded_abstract']['tokens'], dtype=np.float32).reshape(1,-1))
 
 def callback(ch, method, properties, body):
 
     submission = DocumentsSubmission()
     submission.ParseFromString(body)
     print(submission.abstract)
+    
+    authors = []
+    similarities = []
 
     bert_client = BertClient(ip="bert-server")
+
+    abstract = bert_client.encode(submission.abstract, show_tokens=False, is_tokenized=False)
+
+    print(abstract)
+
+    for i in range(len(data)):
+        cur_vector = np.asarray(data[i]['encoded_abstract']['tokens'], dtype=np.float32)
+        cur_id = data[i]['id']
+        cos_sim = cosine_similarity(abstract.reshape(1,-1), cur_vector.reshape(1,-1))
+        
+        authors.append(cur_id)
+        similarities.append(-cos_sim)
+
+    result = [x for _, x in sorted(zip(similarities, authors))]
+    
+    result = result[:5]
+
+    res = requests.post(f"worker_api/v1/submissions/{submission.id}",
+                        headers={f"{os.environ['SECRET_HEADER']}": os.environ['SECRET_TOKEN']},
+                        json={'professor_submission_ids': result})
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
